1b536293017dfb2d882a81361fe335e4e6b62e47
Subir Bhakat <smartsubir2010@gmail.com>
2023-11-30 14:24:55 +0530

    venv added

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='MLOps',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Subir',
+author_email='smartsubir2010@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
